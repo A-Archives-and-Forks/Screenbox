f4d0d6f35ec375c9b65e540f20aeb34a1c900d11
Tung Huynh <tung75605@gmail.com>
2025-01-02 23:32:36 -0800

    add an option to change the SMTC update interval min 1s, max 6s, default 5s

--- Screenbox.Core/Services/ISettingsService.cs
+++ Screenbox.Core/Services/ISettingsService.cs
@@ -19,6 +19,7 @@ namespace Screenbox.Core.Services
         string GlobalArguments { get; set; }
         bool AdvancedMode { get; set; }
         bool UseMultipleInstances { get; set; }
+        double SmtcUpdateInterval { get; set; }
         string LivelyActivePath { get; set; }
         MediaPlaybackAutoRepeatMode PersistentRepeatMode { get; set; }
     }
